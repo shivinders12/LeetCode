9d5c9bf2cc4c64b372cde30e00355920d83c1896
shivinders12 <singhshivinder.12@gmail.com>
2026-08-16 09:10:38 +0530

    Add solution for #88 Merge Sorted Array (C++)

---
+++ 0088-merge-sorted-array/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+        vector<int> ans;
+        int i = 0, j = 0;
+
+        while(i < m && j < n)
+        {
+            if(nums1[i] <= nums2[j])
+            {
+                ans.push_back(nums1[i]);
+                i++;
+            }
