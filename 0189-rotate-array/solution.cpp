c198874e87e760fb8a8a051632c6844478853e83
shivinders12 <singhshivinder.12@gmail.com>
2026-08-16 09:09:27 +0530

    Add solution for #189 Rotate Array (C++)

--- 0189-rotate-array/solution.cpp
+++ 0189-rotate-array/solution.cpp
@@ -1,14 +1,15 @@
-class Solution {
-public:
-    void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-
-
-        if(k>n){
-        k=k%n;
-        }
-        reverse(nums,0,n-1);
         reverse(nums,0,k-1);
         reverse(nums,k,n-1);
 
         for(int index : nums)
+        {
+            cout<<index;
+        }
+
+
+    }
+    void reverse(vector<int>& nums,int start, int end)
+    {
+        while(start<end){
+            int temp =nums[start];
+            nums[start]=nums[end];
