5a7763ad24c07dfc3a090018de55546ea255885c
shivinders12 <singhshivinder.12@gmail.com>
2026-08-16 10:04:45 +0530

    Add solution for #242 Valid Anagram (C++)

--- 0242-valid-anagram/solution.cpp
+++ 0242-valid-anagram/solution.cpp
@@ -1,15 +1,1 @@
-        for(int i=0;i<s.length();i++)
-        }
-        vector<int> asc1(s.length());
-        vector<int> asc2(t.length());
-        {
-            return false;
-        // if(asc1==asc2) return true;
-        // else return false;
-        if(s.length()!=t.length())
-        // {
-        //     asc2= asc2 + t[i];
-        // }
-        //     asc1= asc1 + s[i];
-        // }
-        // for(int i=0;i<t.length();i++)
+
