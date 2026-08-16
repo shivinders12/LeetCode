a6baa25d35b7f43968b01a01176ed681658455c0
shivinders12 <singhshivinder.12@gmail.com>
2026-08-16 09:43:47 +0530

    Add solution for #162 Find Peak Element (C++)

--- 0162-find-peak-element/solution.cpp
+++ 0162-find-peak-element/solution.cpp
@@ -1,1 +1,14 @@
-// Solution code
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 1)
+            return 0;
+
+        int s = 0;
+        int e = n - 1;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
