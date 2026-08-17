39562b4e618f262c449fc5630e63d86cf65899c7
shivinders12 <singhshivinder.12@gmail.com>
2026-08-17 12:07:01 +0530

    Add solution for #796 Rotate String (C++)

---
+++ 0796-rotate-string/solution.cpp
@@ -0,0 +1,14 @@
+                    found = false;
+                    break;
+                }
+            }
+
+            if (found)
+                if (temp[i + j] != goal[j]) {
+
+            for (int j = 0; j < s.length(); j++) {
+            bool found = true;
+        for (int i = 0; i < s.length(); i++) {
+
+        string temp = s + s;
+
