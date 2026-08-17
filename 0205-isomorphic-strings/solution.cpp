84c14952e474d373b29feacaba6e26d33676c189
shivinders12 <singhshivinder.12@gmail.com>
2026-08-17 21:49:19 +0530

    Add solution for #205 Isomorphic Strings (C++)

---
+++ 0205-isomorphic-strings/solution.cpp
@@ -0,0 +1,14 @@
+                return false;
+
+            mp[s[i]] = t[i];
+            used[t[i]] = true;
+        }
+        else {
+            // s[i] is already mapped
+            if (mp[s[i]] != t[i])
+            if (used[t[i]])
+            // s[i] is not mapped yet
+        if (mp.find(s[i]) == mp.end()) {
+
+    for (int i = 0; i < s.length(); i++) {
+
