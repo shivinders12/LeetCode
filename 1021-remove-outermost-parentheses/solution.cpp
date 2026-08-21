7d8bcd6d439fa191fa709910e149d534c5be519f
shivinders12 <singhshivinder.12@gmail.com>
2026-08-21 11:38:56 +0530

    Add solution for #1021 Remove Outermost Parentheses (C++)

--- 1021-remove-outermost-parentheses/solution.cpp
+++ 1021-remove-outermost-parentheses/solution.cpp
@@ -1,25 +1,23 @@
-            }
-            else{
-                end++;
-                std.pop();
                 if(std.empty()==true && st!=end)
                 {   
                     for(int i=st+1;i<end;i++)
                     {
                     
-                end++;
-                std.push(s[i]);
-            {
-            if(s[i]=='(')
-        for(int i=0;i<s.length();i++)
-        {
                         ans= ans + s[i];
                     }
                     st=i+1;
                     end=i;
                 }
-        int end=-1;
-        int st=0;
-        stack<char> std;
-        string ans;
 
+            }
+
+
+        }
+        return ans;
+        
+    }
+};
+            }
+            else{
+                end++;
+                std.pop();
