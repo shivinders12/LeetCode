c542865c2faac0888a6f4e00add38a0298a600c7
shivinders12 <singhshivinder.12@gmail.com>
2026-08-22 10:33:12 +0530

    Add solution for #1781 Sum of Beauty of All Substrings (C++)

---
+++ 1781-sum-of-beauty-of-all-substrings/solution.cpp
@@ -0,0 +1,19 @@
+
+    }
+
+        return ans;
+        }
+                int beauty= getmaxfreq(freq)-getminfreq(freq);
+                ans=ans+beauty;
+            }
+                freq[s[j] - 'a']++;
+            for(int j=i;j<s.length();j++)
+            {
+            vector<int> freq(26,0);
+        for(int i=0;i<s.length();i++)
+        {
+        int ans =0;
+        
+    int beautySum(string s) {
+public:
+class Solution {
