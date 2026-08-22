898fa443c257e193dad7dc6fcfc50c22ae0f4441
shivinders12 <singhshivinder.12@gmail.com>
2026-08-22 15:52:16 +0530

    Add solution for #1903 Largest Odd Number in String (C++)

---
+++ 1903-largest-odd-number-in-string/solution.cpp
@@ -0,0 +1,21 @@
+        }
+            {
+                end=i;
+            }
+                break;
+        if(end == -1) return "";
+       
+class Solution {
+public:
+    string largestOddNumber(string num) {
+
+        int n=num.length();
+        int end=-1;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(num[i]=='1' || num[i]=='3' || num[i]=='5' || num[i]=='7' || num[i]=='9')
+        
+
+        return num.substr(0, end + 1);
+    }
+};
