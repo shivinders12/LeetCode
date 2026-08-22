ebdc8318f9bc64c44d1f33167658fb55dfe3285a
shivinders12 <singhshivinder.12@gmail.com>
2026-08-22 15:14:33 +0530

    Add solution for #1614 Maximum Nesting Depth of the Parentheses (C++)

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/solution.cpp
@@ -0,0 +1,23 @@
+        
+        
+        {
+            if(s[i]=='(')
+        }
+            {
+                count++;
+            }
+            else if(s[i]==')')
+            {
+                count --;
+            }
+                if(count>max) {
+                    max=count;
+                }
+        return max;
+        int count =0;
+        int max=0;
+
+public:
+    int maxDepth(string s) {
+class Solution {
+        for(int i=0;i<s.length();i++)
