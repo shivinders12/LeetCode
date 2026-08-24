9dcbe39101599fe6f3171e9a18825a04d3d3d1b1
shivinders12 <singhshivinder.12@gmail.com>
2026-08-24 11:02:28 +0530

    Add solution for #2 Add Two Numbers (C++)

--- 0002-add-two-numbers/solution.cpp
+++ 0002-add-two-numbers/solution.cpp
@@ -1,14 +1,3 @@
-                int sum = t2->val + carry;
-
-                int digit = sum % 10;
-                carry = sum / 10;
-
-                temp->next = new ListNode(digit);
-                temp = temp->next;
-
-                t2 = t2->next;
-            }
-
             while (t2 != nullptr) {
                 temp->next = new ListNode(digit);
                 temp = temp->next;
@@ -22,3 +11,14 @@
                 int digit = sum % 10;
 
                 int sum = t1->val + carry;
+
+                temp = temp->next;
+
+                t1 = t1->next;
+                t2 = t2->next;
+            }
+
+            while (t1 != nullptr) {
+                carry = sum / 10;
+
+                temp->next = new ListNode(digit);
