bd9558e71158d7a220a83c42c5b629cc00905cd9
shivinders12 <singhshivinder.12@gmail.com>
2026-08-24 15:04:38 +0530

    Add solution for #142 Linked List Cycle II (C++)

--- 0142-linked-list-cycle-ii/solution.cpp
+++ 0142-linked-list-cycle-ii/solution.cpp
@@ -1,1 +1,19 @@
-p
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+    ListNode* curr = head;
+    ListNode* prev = head;
+
+    unordered_set<ListNode*> visited;
+    while (curr != nullptr) {
+
+    if (visited.find(curr) != visited.end()) {
+        // This node was already visited
