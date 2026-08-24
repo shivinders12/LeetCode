ad1d3b94ebd1a5567445af584bb77d2423fd9854
shivinders12 <singhshivinder.12@gmail.com>
2026-08-24 11:09:31 +0530

    Add solution for #2 Add Two Numbers (C++)

--- 0002-add-two-numbers/solution.cpp
+++ 0002-add-two-numbers/solution.cpp
@@ -1,1 +1,20 @@
 
+            if (l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+
+            if (l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            temp->next = new ListNode(sum % 10);
+            carry = sum / 10;
+
+            temp = temp->next;
+        }
+
+        return head->next;
+    }
+};
