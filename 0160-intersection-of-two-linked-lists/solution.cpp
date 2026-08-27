036b4250c11eaaa97b7e34ef7d74322eb1b10e2d
Shivinder Singh <singhshivinder.12@gmail.com>
2026-08-27 14:43:11 +0530

    Add solution for #160 Intersection of Two Linked Lists (C++)

---
+++ 0160-intersection-of-two-linked-lists/solution.cpp
@@ -0,0 +1,19 @@
+
+      
+        temp = headB;
+
+        while (temp != nullptr) {
+
+            for (int i = 0; i < v.size(); i++) {
+                if (temp == v[i]) {
+                    return temp;
+                }
+            }
+
+            temp = temp->next;
+        }
+
+        return nullptr;
+        
+    }
+};
