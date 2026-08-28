8809d7a14069a6d8393f038029d0d8afd87fb662
Shivinder Singh <singhshivinder.12@gmail.com>
2026-08-28 21:23:31 +0530

    Add solution for #21 Merge Two Sorted Lists (C++)

--- 0021-merge-two-sorted-lists/solution.cpp
+++ 0021-merge-two-sorted-lists/solution.cpp
@@ -1,4 +1,3 @@
-                temp->next = list1;
                 list1 = list1->next;
             }
             else {
@@ -9,7 +8,6 @@
             temp = temp->next;
         }
 
-            if (list1->val <= list2->val) {
      
         if (list1 != nullptr)
             temp->next = list1;
@@ -17,8 +15,5 @@
             temp->next = list2;
 
         return dummy->next;
-
-        while (list1 != nullptr && list2 != nullptr) {
     }
 };
-
