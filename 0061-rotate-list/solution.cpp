6680e568f8b25bf66ad73d489b740e90106bacd7
Shivinder Singh <singhshivinder.12@gmail.com>
2026-08-31 14:42:29 +0530

    Add solution for #61 Rotate List (C++)

--- 0061-rotate-list/solution.cpp
+++ 0061-rotate-list/solution.cpp
@@ -1,1 +1,25 @@
-// Solution code
+
+       while(temp->next!=nullptr)
+       {
+        temp= temp->next;
+        node++;
+       }
+       temp->next=head;
+       ListNode* t2=head;
+       ListNode* t3=head->next;
+       
+        k = k % node;
+        if (k == 0)
+            return head;
+       for(int i=0;i<(node-k)-1;i++)
+       {
+        t2=t2->next;
+        t3=t3->next;
+
+       }
+       t2->next=nullptr;
+       int node=1;
+       ListNode* temp=head;
+        
+    ListNode* rotateRight(ListNode* head, int k) {
+public:
