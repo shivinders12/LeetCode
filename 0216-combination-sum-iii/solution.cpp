115b3a3c7cf080c80515531e0496fafca954b4fe
Shivinder Singh <singhshivinder.12@gmail.com>
2026-09-05 11:15:23 +0530

    Add solution for #216 Combination Sum III (C++)

---
+++ 0216-combination-sum-iii/solution.cpp
@@ -0,0 +1,25 @@
+
+        }
+
+    }
+            ans.pop_back();
+            solve(k,k1+1,n,help,ans,sol,sum+help[i],i+1);
+            ans.push_back(help[i]);
+        {
+            if(sum+help[i]>n || k1>k) break;
+
+        for(int i=index;i<help.size();i++)
+        } 
+
+            sol.push_back(ans);
+            return;
+        if(k1==k && sum==n)
+        {
+        
+    index)
+
+    {   
+
+    void solve(int k,int k1, int n, vector<int>& help,vector<int>& ans,vector<vector<int>>& sol,int sum,int 
+class Solution {
+public:
