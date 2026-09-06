c80bdab58a67b4573d923a4aaafc97f5a7c21198
Shivinder Singh <singhshivinder.12@gmail.com>
2026-09-06 11:27:04 +0530

    Add solution for #509 Fibonacci Number (C++)

---
+++ 0509-fibonacci-number/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+
+    int solve(int n)
+    int fib(int n) {
+
+        int ans= solve(n);
+        
+    {
+        if(n==1) return 1;
+    }
+    }
+        return solve(n-1)+solve(n-2);
+        return ans;
+        if(n==0) return 0;
+};
