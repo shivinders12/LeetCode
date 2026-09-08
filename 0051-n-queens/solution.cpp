59e26aa95c8c629e30c11c57bc3687942dd28e9a
Shivinder Singh <singhshivinder.12@gmail.com>
2026-09-08 13:28:02 +0530

    Add solution for #51 N-Queens (C++)

---
+++ 0051-n-queens/solution.cpp
@@ -0,0 +1,25 @@
+
+            i--;
+            j--;
+        }
+                return false;
+
+        {
+            if(help[i][j] == "Q")
+        int j = col - 1;
+
+        while(i >= 0 && j >= 0)
+       
+        int i = row - 1;
+        }
+
+                return false;
+            if(help[i][col] == "Q")
+        for(int i = 0; i < row; i++)
+        {
+    {
+        
+
+    bool isvalid(vector<vector<string>>& help, int row, int col, int n)
+class Solution {
+public:
