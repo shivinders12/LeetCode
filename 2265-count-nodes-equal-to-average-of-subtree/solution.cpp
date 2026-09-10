cca215953c975b06b1a846d4d02174e55eb7d1af
Shivinder Singh <singhshivinder.12@gmail.com>
2026-09-10 16:30:47 +0530

    Add solution for #2265 Count Nodes Equal to Average of Subtree (C++)

--- 2265-count-nodes-equal-to-average-of-subtree/solution.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/solution.cpp
@@ -1,13 +1,3 @@
-        int avg = sum / count;
-
-        if(temp->val == avg)
-            ans++;
-
-        return {sum, count};
-    }
-
-    int solve(int &ans,TreeNode* temp)
-    {
         if(temp==nullptr) return 0;
         int x=3;
         if(temp->left==nullptr) x--;
@@ -16,9 +6,19 @@
         int leftsum=solve(ans,temp->left);
         int sum = temp->val+ leftsum+rightsum;
 
-    }
-
         int avg=sum/x;
         if(avg==temp->val) ans++;
         return sum;
 
+    }
+
+
+class Solution {
+public:
+   int averageOfSubtree(TreeNode* root)
+    {
+        int ans = 0;
+
+        solve(ans, root);
+
+        return ans;
