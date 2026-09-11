0c9e119b738850c58ac63b1e4bd2fb75c7793936
Shivinder Singh <singhshivinder.12@gmail.com>
2026-09-11 11:45:00 +0530

    Add solution for #3483 Unique 3-Digit Even Numbers (C++)

--- 3483-unique-3-digit-even-numbers/solution.cpp
+++ 3483-unique-3-digit-even-numbers/solution.cpp
@@ -1,25 +1,24 @@
+class Solution {
+public:
+    void solve(vector<int>& digits, vector<bool>& used, int& ans, int value, 
+    int count,vector<bool>& check) {
 
+        if (count == 3) {
+
+            if (value % 2 == 0 && check[value]==false)
+                ans++;
+                check[value]=true;
+            return;
+        }
+
+        for (int i = 0; i < digits.size(); i++) {
+
+            if (used[i]) continue;
+            if (count == 0 && digits[i] == 0) continue;
             used[i] = true;
-
-                continue;
-            if (count == 0 && digits[i] == 0)
-            
-            
-            if (used[i])
-                continue;
-
-            solve(digits, used, ans,
-                  value * 10 + digits[i],
-                  count + 1,check);
-
+            solve(digits, used, ans,value * 10 + digits[i],count + 1,check);
             used[i] = false;
         }
     }
 
-        for (int i = 0; i < digits.size(); i++) {
-        }
-
-            return;
-                check[value]=true;
-                ans++;
-            if (value % 2 == 0 && check[value]==false)
+    int totalNumbers(vector<int>& digits) {
