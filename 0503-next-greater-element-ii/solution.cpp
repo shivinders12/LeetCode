c5301bea3f378ea410d69d4803ef553bce1e2546
Shivinder Singh <singhshivinder.12@gmail.com>
2026-09-12 18:41:25 +0530

    Add solution for #503 Next Greater Element II (C++)

--- 0503-next-greater-element-ii/solution.cpp
+++ 0503-next-greater-element-ii/solution.cpp
@@ -1,26 +1,25 @@
+            //     if(nums[j]>nums[i])
+            //     {
+            //         k=nums[j];
+            //         break;
+            //     }
+            // }
             // for(int j=i;j<n;j++)
             // {
             //     if(nums[j]>nums[i])
+            //     {
+            //         k=nums[j];
+            //         break;
+            //     }
             // {
-            //     if(nums[j]>nums[i])
-            // }
-            //     {
-            //         k=nums[j];
-            //     }
-            //     {
-            //         k=nums[j];
-            //     }
-            //         break;
-            //         break;
-            // for(int j=0;j<i;j++)
-            int k=-1;
-            // }
-            ans.push_back(k);
-        }
-
-        return ans;
-    }
             int x=nums[i];
-        {
+            int k=-1;
+            // for(int j=0;j<i;j++)
         for(int i=0;i<n;i++)
+        {
+        vector<int> ans;
+        int n=nums.size();
 
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
