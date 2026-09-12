6e061e230cc1c2ff0f91daade76ed0075bdf6c84
Shivinder Singh <singhshivinder.12@gmail.com>
2026-09-12 09:49:53 +0530

    Add solution for #282 Expression Add Operators (C++)

--- 0282-expression-add-operators/solution.cpp
+++ 0282-expression-add-operators/solution.cpp
@@ -1,1 +1,27 @@
 
+    result += last;
+    return result;
+    }
+
+    void solve(string& num, int target, vector<string>& solution, string ans, int count) {
+        if (count == num.length()) {
+            if (calculate(ans) == target) solution.push_back(ans);
+            return;
+        }
+
+        for (int i = count; i < num.length(); i++) {
+            
+            if (i > count && num[count] == '0') break;
+
+            string x = num.substr(count, i - count + 1); 
+
+            if (count == 0) {
+                solve(num, target, solution, ans + x, i + 1);
+            } else {
+                solve(num, target, solution, ans + "+" + x, i + 1);
+                solve(num, target, solution, ans + "-" + x, i + 1);
+                solve(num, target, solution, ans + "*" + x, i + 1);
+            }
+        }
+    }
+    
