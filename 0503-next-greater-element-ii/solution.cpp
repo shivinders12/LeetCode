f55e04ff2d60ee1a24f4338d50175b4f7f960d1d
Shivinder Singh <singhshivinder.12@gmail.com>
2026-09-12 18:31:05 +0530

    Add solution for #503 Next Greater Element II (C++)

--- 0503-next-greater-element-ii/solution.cpp
+++ 0503-next-greater-element-ii/solution.cpp
@@ -12,10 +12,14 @@
                 }
                     break;
                     break;
-            }
-            ans.push_back(k);
-        }
+            for(int j=0;j<i;j++)
+            int k=-1;
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        vector<int> ans;
+        int n=nums.size();
 
-        return ans;
-    }
-};
+        for(int i=0;i<n;i++)
+        {
+            int x=nums[i];
+class Solution {
+public:
