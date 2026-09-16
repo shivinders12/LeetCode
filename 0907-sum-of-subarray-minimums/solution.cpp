4c0a400daaa85d3745fce3145dfca98e4d3766db
Shivinder Singh <singhshivinder.12@gmail.com>
2026-09-16 18:12:40 +0530

    Add solution for #907 Sum of Subarray Minimums (C++)

--- 0907-sum-of-subarray-minimums/solution.cpp
+++ 0907-sum-of-subarray-minimums/solution.cpp
@@ -1,26 +1,22 @@
 
-            st.pop();
-        while(!st.empty())
-
-        }
-            st.push(i);
-
-            else
-                left[i] = i - st.top();
-            if(st.empty())
-                left[i] = i + 1;
-
-                st.pop();
-
-        vector<int> left(n), right(n);
-        stack<int> st;
-
-     
-        for(int i = 0; i < n; i++) {
-            while(!st.empty() && arr[st.top()] >= arr[i])
-        const long long MOD = 1e9 + 7;
-        int n = arr.size();
-
         for(int i = n - 1; i >= 0; i--) {
             while(!st.empty() && arr[st.top()] > arr[i])
                 st.pop();
+
+            if(st.empty())
+                right[i] = n - i;
+            else
+                right[i] = st.top() - i;
+
+            st.push(i);
+        }
+
+        long long ans = 0;
+
+        for(int i = 0; i < n; i++) {
+            ans = (ans + (long long)arr[i] * left[i] * right[i]) % MOD;
+        }
+
+        return ans;
+    }
+};
