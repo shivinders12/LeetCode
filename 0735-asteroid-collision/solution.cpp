c0348100c26c2e7fbd59b7cb66fdd05ec53d7f23
Shivinder Singh <singhshivinder.12@gmail.com>
2026-09-18 11:09:27 +0530

    Add solution for #735 Asteroid Collision (C++)

---
+++ 0735-asteroid-collision/solution.cpp
@@ -0,0 +1,25 @@
+                else {
+                  
+                    destroyed = true;
+                else if (abs(st.top()) == abs(x)) {
+                    st.pop();
+                    destroyed = true;
+                    break;
+                }
+                    st.pop();
+                }
+                 
+                  
+                
+                if (abs(st.top()) < abs(x)) {
+            bool destroyed = false;
+
+            while (!st.empty() && st.top() > 0 && x < 0) {
+        for (int x : asteroids) {
+            
+        
+        stack<int> st;
+
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
