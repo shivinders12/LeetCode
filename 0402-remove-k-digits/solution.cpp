8ee58f00fccaa3e4e78f404ade306406a59117ad
Shivinder Singh <singhshivinder.12@gmail.com>
2026-09-18 11:10:58 +0530

    Add solution for #402 Remove K Digits (C++)

--- 0402-remove-k-digits/solution.cpp
+++ 0402-remove-k-digits/solution.cpp
@@ -1,1 +1,25 @@
-c
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+
+        string st;
+
+        for (char digit : num) {
+
+            while (!st.empty() &&
+                   st.back() > digit &&
+                   k > 0) {
+                st.pop_back();
+                k--;
+            }
+
+            st.push_back(digit);
+        }
+
+      
+        while (k > 0) {
+            st.pop_back();
+            k--;
+        }
+
+       
