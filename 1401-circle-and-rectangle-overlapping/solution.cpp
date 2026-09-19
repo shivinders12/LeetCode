83bb4a3816a8f88961055a92fd47b2cd898661d3
Shivinder Singh <singhshivinder.12@gmail.com>
2026-09-19 10:23:38 +0530

    Add solution for #1401 Circle and Rectangle Overlapping (C++)

--- 1401-circle-and-rectangle-overlapping/solution.cpp
+++ 1401-circle-and-rectangle-overlapping/solution.cpp
@@ -1,26 +1,25 @@
 
-                return dx * dx + dy * dy <= radius * radius;
-            }
-
      
+    //             int dy = y1 - yCenter;
 
+    //             return dx * dx + dy * dy <= radius * radius;
+    //         }
+    //             int dx = x1 - xCenter;
+    //         else if (yCenter <= y1) {
           
-            else if (yCenter <= y1) {
-                int dx = x1 - xCenter;
-                int dy = y1 - yCenter;
+
+
+    //             return dx * dx + dy * dy <= radius * radius;
+    //         }
+        
+    //     if (xCenter <= x1) {
 
        
-            if (yCenter >= y2) {
-                int dx = x1 - xCenter;
-                int dy = yCenter - y2;
-
-                return dx * dx + dy * dy <= radius * radius;
-            }
+    //         if (yCenter >= y2) {
+    //             int dx = x1 - xCenter;
+    //             int dy = yCenter - y2;
+class Solution {
 public:
     bool checkOverlap(int radius, int xCenter, int yCenter,
                       int x1, int y1, int x2, int y2) {
 
-        
-        if (xCenter <= x1) {
-            else {
-                return x1 - xCenter <= radius;
