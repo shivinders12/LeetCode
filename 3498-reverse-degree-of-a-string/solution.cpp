2b32a1bdc89f874365fae11f92a1afd34d878447
Shivinder Singh <singhshivinder.12@gmail.com>
2026-09-20 09:09:45 +0530

    Add solution for #3498 Reverse Degree of a String (C++)

--- 3498-reverse-degree-of-a-string/solution.cpp
+++ 3498-reverse-degree-of-a-string/solution.cpp
@@ -4,14 +4,14 @@ public:
 
         int ans=0;
         for(int i=0;i<s.length();i++)
-
-        
-        return ans;
         {
             int index=i+1;
-        }
-    }
             int index2='z'-s[i]+1;
             int value = index*index2;
             ans= ans+value;
+        }
+
+        return ans;
+        
+    }
 };
