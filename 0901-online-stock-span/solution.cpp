8f62d767f070f4edb59992ca788b193b9ff3e405
Shivinder Singh <singhshivinder.12@gmail.com>
2026-09-21 15:19:09 +0530

    Add solution for #901 Online Stock Span (C++)

--- 0901-online-stock-span/solution.cpp
+++ 0901-online-stock-span/solution.cpp
@@ -1,1 +1,24 @@
-\
+    vector<int> vt;
+
+    StockSpanner() {}
+
+    int next(int price) {
+        vt.push_back(price);
+        int i = vt.size() - 1;
+
+        while (!st.empty() && vt[st.top()] <= price) {
+            st.pop();
+        }
+
+        int span;
+
+        if (st.empty())
+            span = i + 1;
+        else
+            span = i - st.top();
+
+        st.push(i);
+
+        return span;
+    }
+};
