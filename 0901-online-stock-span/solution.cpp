3ee31605633561d77b8e0033744cb0939f57d187
Shivinder Singh <singhshivinder.12@gmail.com>
2026-09-21 15:15:22 +0530

    Add solution for #901 Online Stock Span (C++)

--- 0901-online-stock-span/solution.cpp
+++ 0901-online-stock-span/solution.cpp
@@ -1,1 +1,25 @@
- 
+    
+    int next(int price) {
+        
+        vt.push_back(price);
+        int x=vt.size();
+        int maxcount=0;
+        int count=0;
+     
+
+        for(int i=x-1;i>=0;i--)
+        {
+            if(vt[i]<=price)
+            {
+                count++;
+                maxcount=max(maxcount,count); 
+            }
+            else{break;}
+        } 
+  
+        return maxcount;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
