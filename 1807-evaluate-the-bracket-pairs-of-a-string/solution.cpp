e38a9f1cd00b4433383f688b81e02ced16ed7dcb
Shivinder Singh <singhshivinder.12@gmail.com>
2026-09-26 09:44:45 +0530

    Add solution for #1807 Evaluate the Bracket Pairs of a String (C++)

---
+++ 1807-evaluate-the-bracket-pairs-of-a-string/solution.cpp
@@ -0,0 +1,25 @@
+
+                
+                while (st.back() != '(') {
+                    key.push_back(st.back());
+                    st.pop_back();
+                string key;
+            else {
+                st.push_back(c);
+            }
+        for (char c : s) {
+
+            if (c != ')') {
+
+        }
+
+        string st;
+public:
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+
+        unordered_map<string, string> mp;
+
+        
+        for (auto &x : knowledge) {
+            mp[x[0]] = x[1];
+class Solution {
